10587d5bada8ac3959a9cc72ad95660d14228a38
jk31 <1906225836@qq.com>
2023-07-11 18:44:07 +0800

    zjk commit LoginActivity.kt

--- app/src/main/java/com/hsy/pinunlock/resetPwdActivity/ResetPwdActivity.kt
+++ app/src/main/java/com/hsy/pinunlock/resetPwdActivity/ResetPwdActivity.kt
@@ -2,13 +2,30 @@ package com.hsy.pinunlock.resetPwdActivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import androidx.core.widget.addTextChangedListener
 import com.hsy.pinunlock.R
+import com.hsy.pinunlock.databinding.ActivityLoginBinding
+import com.hsy.pinunlock.databinding.ActivityResetPwdBinding
 
-class ResetPwdActivity : AppCompatActivity() {
+class ResetPwdActivity : AppCompatActivity() ,IResetView{
+    lateinit var resetPresenter: ResetPresenter
+    lateinit var binding :ActivityResetPwdBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(binding.root)
         // 添加视图绑定 todo
+        binding = ActivityResetPwdBinding.inflate(layoutInflater)
+        var answerText = binding.answerText
+        resetPresenter.setQuestion()
+        binding.answerText.addTextChangedListener {
+            resetPresenter.checkAnswer((it as EditText).text.toString())
+        }
 
-        setContentView(R.layout.activity_reset_pwd)
     }
+
+    override fun setQuestionText(text: String) {
+        binding.questionText.text = text
+    }
+
 }
