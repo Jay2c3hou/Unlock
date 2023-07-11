8d25f8c28d52b92c2d2cea0d0970be1fe95f659b
jk31 <1906225836@qq.com>
2023-07-11 16:15:56 +0800

    zlh commit LoginActivity.kt

--- app/src/main/java/com/hsy/pinunlock/loginActivity/LoginActivity.kt
+++ app/src/main/java/com/hsy/pinunlock/loginActivity/LoginActivity.kt
@@ -1,25 +1,38 @@
 package com.hsy.pinunlock.loginActivity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
+import androidx.core.widget.addTextChangedListener
 import com.hsy.pinunlock.databinding.ActivityLoginBinding
+import com.hsy.pinunlock.resetPwdActivity.ResetPwdActivity
 
 class LoginActivity : AppCompatActivity(), ILoginView {
     lateinit var binding: ActivityLoginBinding
+
     // loginPresenter todo
+    private val loginPresenter = LoginPresenter(this, UserModel())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
         // 给 forgetPwdText添加点击事件，使得点击之后跳转到 resetPwdActivity todo
+        binding.forgetPwdText.setOnClickListener {
+            startActivity(Intent(this, ResetPwdActivity::class.java))
+        }
 
+        // 给 numberText添加 TextListener接口来监听 text值的改变 todo 去网上学这个接口怎么用
+        binding.numberText.addTextChangedListener {
+            loginPresenter.checkPwd((it as EditText).text.toString())
+        }
     }
 
+    //设置提示文本 todo
     override fun setTipText(text: String) {
-        TODO("Not yet implemented")
+        binding.tipText.text = text
     }
-
-    // 给 numberText添加 TextWatcher接口来监听 text值的改变 todo 去网上学这个接口怎么用
-
 }
