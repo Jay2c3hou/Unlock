8cb932750ee8419957dfc20dabf2b2cf473695ca
胡绍鹰 <1619548883@qq.com>
2023-07-11 19:19:42 +0800

    hsy commit

--- app/src/main/java/com/hsy/pinunlock/resetPwdActivity/QuestionModel.kt
+++ app/src/main/java/com/hsy/pinunlock/resetPwdActivity/QuestionModel.kt
@@ -2,7 +2,6 @@ package com.hsy.pinunlock.resetPwdActivity
 
 class QuestionModel {
     // 用 hashMap存储问题和答案 todo 网上学 hashMap怎么用
-    // val question = todo
     val resetMap = hashMapOf(
         "你的名字是" to "name",
         "你的电话是" to "110"
